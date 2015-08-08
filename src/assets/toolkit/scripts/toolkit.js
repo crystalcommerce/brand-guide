2e10e6e6cab57de204fc608e823af9bc026de0af
Andrew Anderson <andrew.anderson@crystalcommerce.com>
2015-08-07 17:37:25 -0700

    Componenet revisions
    Stripping down the default packaged componenets with those from the
    marketing site.

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -3,3 +3,50 @@
  */
 
 'use strict';
+var $ = require('jquery');
+
+//Define Tabber Function
+var tabber = function(triggerContainer, panelContainer, callback){
+
+    //Find the triggers within the specified triggerContainer
+    var triggers = $(triggerContainer).find('[data-tab-trigger]');
+
+    //Find the triggers within the specified panelContainer
+    var panels = $(panelContainer).find('[data-tab-panel]');
+
+    //Bind a click event to the trigger
+    $(triggers).on('click', function(e){
+
+        //Prevent default action (clickthrough)
+        e.preventDefault();
+
+        //Find the name of the panel to activate
+        var panelName = $(this).attr('data-tab-trigger');
+
+        //Remove the active class from the triggers not clicked
+        $(triggers).not(this).removeClass('active');
+
+        //Add the active class to the trigger clicked
+        $(this).addClass('active');
+
+        //Find the panel that we want to be visible using the panelName
+        var activatedPanel = $(panelContainer).find('[data-tab-panel="' + panelName + '"]');
+
+        //Remove the active class from the panels not to be activated
+        $(panels).not(activatedPanel).removeClass('active');
+
+        //Add the active class to the panel to be activated
+        $(activatedPanel).addClass('active');
+
+        //Optional callback
+        if(callback){
+
+            callback();
+
+        }
+
+    });
+
+};
+
+$(document).ready(function(){tabber('[data-tabs]', '[data-tabs]');});
